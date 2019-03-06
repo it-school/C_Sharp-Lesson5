114bdb5f158b266e840520dd87d6ae560b8d554e
it-school <it@osvita.od.ua>
2019-03-06 18:21:55 +0200

    Mar2019

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -11,82 +11,85 @@ namespace Lesson5
         static void Main(string[] args)
         {
             int[] first = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-            int[] second = new int[first.Length];
 
             foreach (var item in first)
             {
                 Console.Write($"{item}, ");
             }
 
-            Array.Copy(first, 3, second, 4, 5);
-            Console.WriteLine("\n\n");
-            foreach (var item in second)
-            {
-                Console.Write($"{item}, ");
-            }
+
+            //Console.WriteLine('\n');
+            //Console.WriteLine(first);
 
 
-            int[,] c = {
-                            { 1, 2, 3 },
-                            { 4, 5, 6 },
-                            { -1, -2, -3 },
-                            { -4, -5, 0 }
-                        };
+            //int[] second = new int[first.Length];
+            //Array.Copy(first, 3, second, 4, 5);
+            //Console.WriteLine("\n\nArray.Copy(first, 3, second, 4, 5);");
+            //foreach (var item in second)
+            //{
+            //    Console.Write($"{item}, ");
+            //}
 
-            Console.WriteLine("\n\n");
-            Random r = new Random();
-            for (int row = 0; row < c.GetLength(0); row++)
-            {
-                for (int column = 0; column < c.GetLength(1); column++)
-                {
-                    c[row, column] = r.Next(-100, 100); // [-100, 100)
-                    Console.Write($"{c[row, column]}\t");
-                }
-                Console.WriteLine();
-            }
 
-            Console.WriteLine("\n\n");
-            int[][] jaggedArray = { new int[3], new int[5], new int[4] };
-            for (int i = 0; i < jaggedArray.GetLength(0); i++)
-                for (int j = 0; j < jaggedArray[i].Length; j++)
-                    jaggedArray[i][j] = r.Next(-10, 10);
+            //int[,] c = {
+            //                { 1, 2, 3 },
+            //                { 4, 5, 6 },
+            //                { -1, -2, -3 },
+            //                { -4, -5, 0 }
+            //            };
 
-            foreach (var arrayRow in jaggedArray)
-            {
-                foreach (var itemsOfRow in arrayRow)
-                    Console.Write($"{itemsOfRow}\t");
-                Console.WriteLine();
-            }
+            //Console.WriteLine("\n\n");
+            //Random r = new Random();
+            //for (int row = 0; row < c.GetLength(0); row++)
+            //{
+            //    for (int column = 0; column < c.GetLength(1); column++)
+            //    {
+            //        c[row, column] = r.Next(-100, 100); // [-100, 100)
+            //        Console.Write($"{c[row, column]}\t");
+            //    }
+            //    Console.WriteLine();
+            //}
 
-            Console.WriteLine();
+            //Console.WriteLine("\n\n");
+            //int[][] jaggedArray = { new int[3], new int[5], new int[4] };
+            //for (int i = 0; i < jaggedArray.GetLength(0); i++)
+            //    for (int j = 0; j < jaggedArray[i].Length; j++)
+            //        jaggedArray[i][j] = r.Next(-10, 10);
 
-            Console.WriteLine("\n\n");
+            //foreach (var arrayRow in jaggedArray)
+            //{
+            //    foreach (var itemsOfRow in arrayRow)
+            //        Console.Write($"{itemsOfRow}\t");
+            //    Console.WriteLine();
+            //}
 
-            int[] december = new int[31];
-            for (int i = 0; i < december.Length; i++)
-            {
-                december[i] = r.Next(-10, 10);
-                Console.Write($"{december[i]}\t" );
 
-            }
-            int day = 1;
-            foreach (var item in december)
-            {
-                if (item < 0)
-                    break;
-                day++;
-            }
-            Console.WriteLine("first cold day:"+day);
+            //Console.WriteLine("\n\n");
 
-            int couter = 0; 
-            foreach(var item in december)
-            {
-                if (item > 0)
-                    
-                    couter++; 
+            //int[] december = new int[31];
+            //for (int i = 0; i < december.Length; i++)
+            //{
+            //    december[i] = r.Next(-10, 10);
+            //    Console.Write($"{december[i]}\t" );
 
-            }
-            Console.WriteLine(couter);
+            //}
+            //int day = 1;
+            //foreach (var item in december)
+            //{
+            //    if (item < 0)
+            //        break;
+            //    day++;
+            //}
+            //Console.WriteLine("first cold day:"+day);
+
+            //int counter = 0; 
+            //foreach(var item in december)
+            //{
+            //    if (item > 0)
+            //        counter++; 
+
+            //}
+            //Console.WriteLine(counter);
         }
     }
 }
