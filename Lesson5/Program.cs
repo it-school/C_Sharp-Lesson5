0dc6af115763740096be3603e761ad6ac079b509
IT-school.com.ua <37939704+it-school@users.noreply.github.com>
2019-03-06 18:26:27 +0200

    Update Program.cs

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -8,6 +8,17 @@ namespace Lesson5
 {
     class Program
     {
+        public static void PrintAr3(string[] Fathers, string[][] Children)
+        {
+            for (int i = 0; i < Fathers.Length; i++)
+            {
+                Console.WriteLine("Отец : {0}; Его дети:", Fathers[i]);
+                for (int j = 0; j < Children[i].Length; j++)
+                    Console.Write(Children[i][j] + " ");
+                Console.WriteLine();
+            }
+        }//PrintAr3
+        
         static void Main(string[] args)
         {
             int[] first = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
@@ -30,6 +41,32 @@ namespace Lesson5
             //    Console.Write($"{item}, ");
             //}
 
+            //int[] bb = new int[100000];
+            //for (int i = 0; i < bb.Length; i++)
+            //{
+            //    bb.SetValue(r.Next(1, 1000), i);
+                // bb[i] = r.Next(1, 1000);
+                //                Console.WriteLine(bb[i]+" ");
+            //}
+            
+            /*
+            int w = 10;
+            String s = "10 7";
+
+            Int32.TryParse(s, out w);
+            
+            try
+            {
+                w = Int32.Parse(s);
+            }
+            catch (Exception e)
+            {
+                w = 0;
+                // Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine(w);
+            */
 
             //int[,] c = {
             //                { 1, 2, 3 },
@@ -63,6 +100,15 @@ namespace Lesson5
             //    Console.WriteLine();
             //}
 
+            //int Fcount = 3;
+            //string[] Fathers = new string[Fcount];
+            //Fathers[0] = "Николай"; Fathers[1] = "Сергей";
+            //Fathers[2] = "Петр";
+            //string[][] Children = new string[Fcount][];
+            //Children[0] = new string[] { "Ольга", "Федор" };
+            //Children[1] = new string[] { "Сергей", "Валентина", "Ира", "Дмитрий", "Ира", "Дмитрий" };
+            //Children[2] = new string[] { "Мария", "Ирина", "Надежда" };
+            //PrintAr3(Fathers, Children);
 
             //Console.WriteLine("\n\n");
 
@@ -71,8 +117,25 @@ namespace Lesson5
             //{
             //    december[i] = r.Next(-10, 10);
             //    Console.Write($"{december[i]}\t" );
-
             //}
+            
+            // Пузырьковая сортировка
+            /*
+            for (int j = 0; j < b.Length - 1; j++)
+            {
+                for (int i = 0; i < b.Length - 1 - j; i++)
+                {
+                    if (b[i] > b[i + 1])
+                    {
+                        t = b[i];
+                        b[i] = b[i + 1];
+                        b[i + 1] = t;
+                    }
+                }
+            }
+            Array.Sort(b);
+            */
+            
             //int day = 1;
             //foreach (var item in december)
             //{
